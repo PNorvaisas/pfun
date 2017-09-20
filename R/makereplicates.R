1315c4b2ca8efb02ee10d5928e1f3720bd8b946d
Povilas <povilas.norvaisas@gmail.com>
2017-09-20 11:26:42 +0100

    makereplicates

---
+++ R/makereplicates.R
@@ -0,0 +1,25 @@
+#' Make a vector with unique replicate indexes
+#'
+#' @param data Table containing columns over which unique repicate indices need to be generated
+#' @keywords makereplicates
+#' @export
+#' @examples
+#' hypothesise()
+
+
+
+makereplicates<-function(data){
+  data$ReplicateUniq<-1
+  alluniq<-FALSE
+  while(!alluniq){
+    print(max(data[,'ReplicateUniq']))
+    duplics<-duplicated(data)
+    alluniq<-all(duplics==FALSE)
+    if (!alluniq) {
+      data[duplics,'ReplicateUniq']<-max(data[duplics,'ReplicateUniq'])+1
+    }
+  }
+  return(data$ReplicateUniq)
+}
+
+
