f23741bc248b59eda4abe45caeeeb3a180d6a01f
Povilas <povilas.norvaisas@gmail.com>
2018-02-09 16:40:14 +0000

    Updated contrasts

--- R/read.contrasts.R
+++ R/read.contrasts.R
@@ -8,25 +8,25 @@
 #' @export
 #' @examples
 #' read.contrasts()
-#' 
-#' 
+#'
+#'
 
 
 read.contrasts<-function(cfile,csheet,samples.selected,variables=c()) {
-  cont.table<-read.and.clean(cfile,csheet)
-  cont.table$`FALSE`<-NULL
+  cont.table<-readxl::read_xlsx(cfile,sheet=csheet)
+  #cont.table$`FALSE`<-NULL
   rownames(cont.table)<-cont.table$Contrast
   contrasts<-cont.table$Contrast
-  
+
 
   #print(mval)
-  
+
   #Find columns which define samples
   #samples.all<-colnames(cont.table)[apply(cont.table,2,function(x) length(setdiff(x,c('-1','0','1')))==0)]
   samples.all<-colnames(cont.table)[apply(cont.table,2,function(x) all(!grepl("[a-zA-Z]",x)))]
   cont.table[,samples.all] <- sapply(cont.table[, samples.all], as.numeric)
-  
-  
+
+
   if ('m' %in% samples.all){
     mval<-TRUE
   } else {
@@ -35,11 +35,11 @@ read.contrasts<-function(cfile,csheet,samples.selected,variables=c()) {
   samples.all<-setdiff(samples.all,c('m',variables))
 
 
-  
+
   samples.found<-intersect(samples.all,samples.selected)
   samples.missing<-setdiff(samples.all,samples.found)
-  
-  
+
+
   if (length(samples.selected)>length(samples.found)){
     print("Some of the selected samples not present in the table!")
     print(setdiff(samples.selected,samples.found))
@@ -47,31 +47,31 @@ read.contrasts<-function(cfile,csheet,samples.selected,variables=c()) {
     print("All samples found!")
     print(samples.found)
   }
-  
+
   #print(samples.missing)
-  
+
   #Find contrasts that do not require missing samples
   cont.use<-contrasts[apply(cont.table[,samples.found],1,function(x) any(x==1) | any(x==-1) )]
-  
+
   if ( length(samples.missing)>0 ) {
     cont.nomiss<-contrasts[apply(cont.table[,samples.missing],1,function(x) all(x==0) )]
     cont.clean<-intersect(cont.nomiss,cont.use)
   } else {
     cont.clean<-cont.use
   }
-  
+
 
   cont.table.clean<-cont.table[cont.clean,,drop=FALSE]
-  
+
   if (mval==TRUE) {
     cont.table.selected<-cont.table[cont.clean, c(samples.found,'m'),drop=FALSE]
   } else {
     cont.table.selected<-cont.table[cont.clean, samples.found,drop=FALSE]
   }
- 
-  
+
+
   cont.matrix<-as.matrix(cont.table.selected)
-  
+
   return(list("Contrasts.table"=cont.table.clean,"Contrasts.matrix"=cont.matrix))
 }
 
