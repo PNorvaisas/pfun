30e6dea6f8d8899283a846dea79e65aa121068ac
Povilas <povilas.norvaisas@gmail.com>
2018-03-22 17:32:19 +0000

    theme

--- R/theme_Publication.R
+++ R/theme_Publication.R
@@ -30,7 +30,7 @@ theme_Publication <- function(base_size=14) {
            #legend.direction = "horizontal",
            #legend.key.size= unit(0.2, "cm"),
            #legend.margin = unit(0, "cm"),
-           legend.title = element_text(face="italic"),
+           legend.title = element_text(),#face="italic"
            #plot.margin=unit(c(10,5,5,5),"mm"),
            strip.background=element_rect(colour="#f0f0f0",fill="#f0f0f0"),
            strip.text = element_text(face="bold")
