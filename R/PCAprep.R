751ae81a0df0bddd3dfae0a2a3e3abc8816f4f21
Povilas <povilas.norvaisas@gmail.com>
2018-05-02 14:59:25 +0100

    PCAprep updates

--- R/PCAprep.R
+++ R/PCAprep.R
@@ -5,6 +5,7 @@
 #' @param feature feature in rows
 #' @param value Value to be used in heatmap
 #' @param info Table with descriptors of interest
+#' @param grouping Groups of observations for ellipses
 #' @param scalink Scale feature for hierarchical clustering?
 #' @param dmethod Distance measure for hierarchical clustering
 #' @param cmethod Clustering method for hierarchical clustering
@@ -12,7 +13,7 @@
 #' @export
 #' @examples
 #' PCAprep(data,ID,feature,value,info,cols=list(),scalesel='row',rnames=FALSE,reorder.rows=TRUE,reorder.columns=TRUE,name='Z-score',title="Heatmap",cdr="euclidean",cdc="euclidean",cmr="ward.D2",cmc="ward.D2",rmax=10)
-PCAprep<-function(data,ID,feature,value,info,scaling=TRUE,dmethod="euclidean",cmethod="ward.D2") {
+PCAprep<-function(data,ID,feature,value,info,grouping="All",scaling=TRUE,dmethod="euclidean",cmethod="ward.D2") {
   
   pcashape<-data %>%
     select_(ID,feature,value) %>%
@@ -32,13 +33,18 @@ PCAprep<-function(data,ID,feature,value,info,scaling=TRUE,dmethod="euclidean",cm
   
   pca <- prcomp(pcashape,
                 center = TRUE,
-                scale. = TRUE)
+                scale = TRUE)
   
   pcadata<-data.frame(pca$x) %>%
     rownames_to_column(ID) %>%
     left_join(info)
   
-  groups<-base::setdiff(colnames(info),ID)
+  if (grouping=="All") {
+    groups<-base::setdiff(colnames(info),ID)
+  } else {
+    groups<-grouping
+  }
+  
   
   pcaresult<-summary(pca)$importance
   PC1prc<-round(pcaresult['Proportion of Variance',][[1]]*100,0)
